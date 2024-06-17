110104def963be736159241279d00ec98afef00b
Saurabh Chavan <124519125+SaurabhCS-29@users.noreply.github.com>
2024-06-17 07:09:01 +0000

    Conversion opretions

---
+++ 01_Basics/04_conversionOpreation.js
@@ -0,0 +1,25 @@
+let score = "3acs3"
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+// "33" => 33
+// "33acs" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "js"
+
+let boolenIsLoggedIn = Boolean(isLoggedIn)
+console.log(boolenIsLoggedIn);
+
+// 1 => true; 0 => flase
+// "" => false
+// "js" => true
+
+let someNumber = 34
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber)
