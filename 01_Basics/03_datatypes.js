790f90e8a1b8c2fbfd8bee56083f54f972ccbf2f
Saurabh Chavan <124519125+SaurabhCS-29@users.noreply.github.com>
2024-06-17 06:45:34 +0000

    Data types in javascript

---
+++ 01_Basics/03_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version 
+
+//alert( 3+3) //we are using nodjs, not browser
+
+console.log(3+3)
+console.log("JavaScript.")
+
+let name = "Saurabh" 
+let age = 24
+let isLoggedIn = false
+
+// number => 2 to power 53
+// BigInt 
+// string => ""
+// null => standalone value
+// undefined => 
+// boolen => true/false
+// symbol =>unique
+
+
+// object
+console.log(typeof"saurabh");
+
+console.log(typeof undefined) // undefind
+console.log(typeof null) // object
