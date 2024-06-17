b2741ef2135d5ee7c6ea58e1777f468feff83964
Saurabh Chavan <124519125+SaurabhCS-29@users.noreply.github.com>
2024-06-17 07:24:39 +0000

    Operations

--- 01_Basics/04_conversionOpreation.js
+++ 01_Basics/04_conversionOpreation.js
@@ -1,25 +1,47 @@
-let score = "3acs3"
-console.log(typeof score);
+// let score = "3acs3"
+// console.log(typeof score);
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
 
-// "33" => 33
-// "33acs" => NaN
-// true => 1; false => 0
+// // "33" => 33
+// // "33acs" => NaN
+// // true => 1; false => 0
 
-let isLoggedIn = "js"
+// let isLoggedIn = "js"
 
-let boolenIsLoggedIn = Boolean(isLoggedIn)
-console.log(boolenIsLoggedIn);
+// let boolenIsLoggedIn = Boolean(isLoggedIn)
+// console.log(boolenIsLoggedIn);
 
-// 1 => true; 0 => flase
-// "" => false
-// "js" => true
+// // 1 => true; 0 => flase
+// // "" => false
+// // "js" => true
 
-let someNumber = 34
+// let someNumber = 34
 
-let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber)
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber)
+
+
+// ************* Operations ***************
+
+let value  = 3
+let negValue = -value
+// console.log(negValue)
+
+let str1 = "hello"
+let str2 = "js"
+
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2)
+console.log(1 +"2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter)
